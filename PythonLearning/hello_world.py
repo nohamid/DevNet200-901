a4208c3b2a13581b636038fe25a620990f3cf71e
Nomaan <nomaan.hamid@gmx.de>
2024-10-04 15:47:30 +0200

    umbenannt

--- PythonLearning/hello_world.py
+++
@@ -1,6 +0,0 @@
-
-fn = "nomaan"
-ln = "hamid"
-
-full_name = f"{fn} {ln}"
-print (full_name)
---
+++ PythonLearning/strings.py
@@ -0,0 +1,30 @@
+from colorama import Fore
+fn = "Nomaan"
+ln = "Hamid"
+
+full_name = f"{fn} {ln}"
+print ("Name wurde zusammengesetzt:")
+print (full_name)
+
+print ("Name wurde kleingeschrieben:")
+print (full_name.lower())
+
+print ("Name wurde grossgeschrieben:")
+print (full_name.upper())
+
+print ("Add Tab:")
+print ("\t(full_name)")
+
+# Work with Whitespaces:
+
+ws = ' hello '
+print(ws)
+# Remove left Whitespace (rstrip would be right)
+wsl = ws.lstrip()
+print (wsl)
+
+# Calculate 
+
+number = 4 + 1
+print (number)
+ 
